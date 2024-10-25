649fb79a7b986c5394addc8e6908a528b51dedf9
Pedro Rodrigues <pedro@bbde.org>
2024-10-25 14:24:45 +0100

    Add README and docker-compose for apps

---
+++ README.md
@@ -0,0 +1,35 @@
+# Running the application
+
+Both the frontend and backend applications have `Dockerfile`s to create docker images.
+The easiest way to run is as follows:
+
+```bash
+$ docker-compose up
+```
+
+This will attempt to pull all images, fail on the apps, build them, and then start 5 containers:
+
+- Postgres service
+- Adminer
+- Rails Backend
+- Compiled React Frontend running on Nginx
+- Setup Rails backend app
+  - This is a pre-requisite for the Rails backend app which runs:
+    - `rails db:create`
+    - `rails db:migrate`
+    - `rails db:seed`
+
+Both the Rails backend and Setup Rails backend depend on Postgres being healthy, so it might take a few seconds before the healthcheck is positive.
+
+The Rails backend app will also only run after the Setup Rails backend app finishes successfully.
+
+If everything is up and running, you should see the frontend when accessing http://localhost:5173/ on your browser.
+
+Once you're done with it, to tear everything down, just stop docker compose, and then:
+
+```bash
+docker-compose stop && \
+docker-compose rm -f && \
+docker volume rm -f aroundhome_postgres-data && \
+docker image rm -f aroundhome-{api,frontend}:tech-challenge
+```
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,7 +10,6 @@ require 'faker'
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Partner.destroy_all
 
 # These values are used to place all the points roughly inside Germany
 LATITUDE_RANGE = 47.638179..53.590240
@@ -24,44 +23,51 @@ def random_longitude
   Faker::Number.within range: LONGITUDE_RANGE
 end
 
-100.times do |i|
-  Partner.create! do |p|
-    address = Faker::Address.full_address_as_hash(:full_address, :postcode, :city, :country_code, :country)
+if Partner.count.zero?
+  100.times do |i|
+    Partner.create! do |p|
+      address = Faker::Address.full_address_as_hash(:full_address, :postcode, :city, :country_code, :country)
 
-    p.name = Faker::Company.unique.name
+      p.name = Faker::Company.unique.name
 
-    p.materials << :wood if Faker::Number.within(range: 1..2).even?
-    p.materials << :carpet if Faker::Number.within(range: 1..2).even?
-    p.materials << :tiles if Faker::Number.within(range: 1..2).even?
+      p.materials << :wood if Faker::Number.within(range: 1..2).even?
+      p.materials << :carpet if Faker::Number.within(range: 1..2).even?
+      p.materials << :tiles if Faker::Number.within(range: 1..2).even?
 
-    p.street_address = address[:full_address]
-    p.postcode = address[:postcode]
-    p.city = address[:city]
-    p.country_code = address[:country_code]
-    p.country = address[:country]
-    p.latitude = random_latitude
-    p.longitude = random_longitude
-    p.operating_radius = Faker::Number.within(range: 10.0e3..1000.0e3) # metres
-    p.rating_score = Faker::Number.within(range: 0..5)
-    p.rating_count = Faker::Number.within(range: 1..1000)
+      p.street_address = address[:full_address]
+      p.postcode = address[:postcode]
+      p.city = address[:city]
+      p.country_code = address[:country_code]
+      p.country = address[:country]
+      p.latitude = random_latitude
+      p.longitude = random_longitude
+      p.operating_radius = Faker::Number.within(range: 10.0e3..1000.0e3) # metres
+      p.rating_score = Faker::Number.within(range: 0..5)
+      p.rating_count = Faker::Number.within(range: 1..1000)
+    end
   end
+else
+  puts 'Skipping partner creation, already exist'
 end
 
-# Min ,
-# Max ,
+if Customer.count.zero?
+  100.times do |i|
+    Customer.create! do |c|
+      address = Faker::Address.full_address_as_hash(:full_address, :postcode, :city, :country_code, :country)
 
-100.times do |i|
-  Customer.create! do |c|
-    address = Faker::Address.full_address_as_hash(:full_address, :postcode, :city, :country_code, :country)
+      c.name = Faker::Name.unique.name
 
-    c.name = Faker::Name.unique.name
-
-    c.street_address = address[:full_address]
-    c.postcode = address[:postcode]
-    c.city = address[:city]
-    c.country_code = address[:country_code]
-    c.country = address[:country]
-    c.latitude = random_latitude
-    c.longitude = random_longitude
+      c.street_address = address[:full_address]
+      c.postcode = address[:postcode]
+      c.city = address[:city]
+      c.country_code = address[:country_code]
+      c.country = address[:country]
+      c.latitude = random_latitude
+      c.longitude = random_longitude
+    end
   end
+else
+  puts 'Skipping customer creation, already exist'
 end
+
+puts "Database seeding completed successfully"
